a76585a51c081d4b9171d6b78ddd53e7fe1f8bb3
simbu_000 <simbu_000@132.231.214.81>
2017-08-02 15:51:58 +0200

    Update

--- src/SCAPI/UnitUtil/Vector.java
+++ src/SCAPI/UnitUtil/Vector.java
@@ -4,131 +4,143 @@ import bwapi.Game;
 import bwapi.Position;
 
 public class Vector {
-	protected Position origin;
-	protected Position pos;
+    protected Position origin;
+    protected Position pos;
 
-	/**
-	 * @param pos
-	 */
-	public Vector(Position pos) {
-		this(new Position(0, 0), pos);
-	}
+    /**
+     * @param pos
+     */
+    public Vector(Position pos) {
+	this(new Position(0, 0), pos);
+    }
 
-	/**
-	 * @param origin
-	 * @param pos
-	 */
-	public Vector(Position origin, Position pos) {
-		this.origin = origin;
-		this.pos = pos;
-	}
-	
-	public Vector(Vector origin, Vector pos) {
-		this.origin = origin.toPosition();
-		this.pos = pos.toPosition();
-	}
-	
-	public int product(Vector rhs) {
-		Position p_lhs = toPosition();
-		Position p_rhs = rhs.toPosition();
-		return p_lhs.getX() * p_rhs.getX() + p_lhs.getY() + p_rhs.getY();
-	}
-	
-	public double length() {
-		Position p = toPosition();
-		return Math.sqrt(p.getX()^2 + p.getY()^2);
-	}
-	
-	public Vector normalized() {
-		return scale(Math.pow(length(), -1));
-	}
-	
-	public double getAngle() {
-		Position l = toOrigin().toPosition();
-		double phi = Math.atan2(l.getY(), l.getX());
-		return ((phi < 0) ? phi + 2 * Math.PI : phi) * 180 / Math.PI;
-	}
+    /**
+     * @param origin
+     * @param pos
+     */
+    public Vector(Position origin, Position pos) {
+	this.origin = origin;
+	this.pos = pos;
+    }
 
-	/**
-	 * @param rhs
-	 * @return
-	 */
-	public Vector add(Vector rhs) {
-		return new Vector(origin, new Position(pos.getX() + rhs.pos.getX(), pos.getY() + rhs.pos.getY()));
-	}
+    public Vector(Vector origin, Vector pos) {
+	this.origin = origin.toPosition();
+	this.pos = pos.toPosition();
+    }
 
-	/**
-	 * @param x
-	 * @param y
-	 * @return
-	 */
-	public Vector add(int x, int y) {
-		return add(new Vector(new Position(x, y)));
-	}
-	
-	/**
-	 * @param scale
-	 * @return
-	 */
-	public Vector scale(double scale) {
-		Vector v = new Vector(origin, new Position(
-				(int) ((origin.getX() + pos.getX()) * scale),
-				(int) ((origin.getY() + pos.getY()) * scale)));
-		return v;
-	}
-	
-	public Vector toOrigin() {
-		Position p = toPosition();
-		return new Vector(
-				new Position(0, 0),
-				new Position(p.getX() - origin.getX(), p.getY() - origin.getY())
-				);
-	}
+    public int product(Vector rhs) {
+	Position p_lhs = toPosition();
+	Position p_rhs = rhs.toPosition();
+	return p_lhs.getX() * p_rhs.getX() + p_lhs.getY() + p_rhs.getY();
+    }
 
-	/**
-	 * @param angle
-	 * @return
-	 */
-	public Vector rotate(double angle) {
-		Position p = toOrigin().toPosition();
-		double newAngle = Math.toRadians(angle);
-		
-		Position rotated = new Position(
-				(int) (p.getX() * Math.cos(newAngle) - p.getY() * Math.sin(newAngle)),
-				(int) (p.getX() * Math.sin(newAngle) + p.getY() * Math.cos(newAngle)));
-		Position shifted = new Position(
-				rotated.getX() + origin.getX(),
-				rotated.getY() + origin.getY());
+    public double length() {
+	Vector v = toOrigin();
+	Position p = v.toPosition();
 
-		return new Vector(origin, shifted);
-	}
+	return Math.sqrt(p.getX() ^ 2 + p.getY() ^ 2);
+    }
 
-	/**
-	 * @param rhs
-	 * @return
-	 */
-	public Vector sub(Vector rhs) {
-		return this.add(rhs.scale(-1.0));
-	}
+    public Vector normalized() {
+	Vector v = toOrigin();
+	v.scale(1 / length());
+	return new Vector(origin,
+		new Position(
+			origin.getX() + v.toPosition().getX(),
+			origin.getY() + v.toPosition().getY()));
+    }
 
-	/**
-	 * @return
-	 */
-	public Position toPosition() {
-		return pos;
-	}
+    public double getAngle() {
+	Position l = toOrigin().toPosition();
+	double phi = Math.atan2(l.getY(), l.getX());
+	return ((phi < 0) ? phi + 2 * Math.PI : phi) * 180 / Math.PI;
+    }
 
-	/**
-	 * @param state
-	 * @param c
-	 */
-	public void draw(Game state, bwapi.Color c) {
-		state.drawLineMap(origin, pos, c);
-		state.drawCircleMap(pos, 2, c);
-		state.drawTextMap(toPosition(), ""+getAngle());
-	}
-	
-	public String toString() {
-		return toPosition().toString();
-	}
+    /**
+     * @param rhs
+     * @return
+     */
+    public Vector add(Vector rhs) {
+	Position p = toOrigin().toPosition();
+	Position rp = rhs.toOrigin().toPosition();
+	Position np = new Position(p.getX() + rp.getX(), p.getY() + rp.getY());
+	return new Vector(origin, new Position(np.getX() + origin.getX(),
+		np.getY() + origin.getY()));
+    }
+
+    /**
+     * @param x
+     * @param y
+     * @return
+     */
+    public Vector add(int x, int y) {
+	return add(new Vector(new Position(x, y)));
+    }
+
+    /**
+     * @param scale
+     * @return
+     */
+    public Vector scale(double scale) {
+	Vector v = toOrigin();
+	Position p = v.toPosition();
+	Position np = new Position((int) (p.getX() * scale),
+		(int) (p.getY() * scale));
+	return new Vector(origin, new Position(np.getX() + origin.getX(),
+		np.getY() + origin.getY()));
+    }
+
+    public Vector toOrigin() {
+	Position p = toPosition();
+	return new Vector(new Position(0, 0), new Position(
+		p.getX() - origin.getX(), p.getY() - origin.getY()));
+    }
+
+    /**
+     * @param angle
+     * @return
+     */
+    public Vector rotate(double angle) {
+	Position p = toOrigin().toPosition();
+	double newAngle = Math.toRadians(angle);
+
+	Position rotated = new Position(
+		(int) (p.getX() * Math.cos(newAngle)
+			- p.getY() * Math.sin(newAngle)),
+		(int) (p.getX() * Math.sin(newAngle)
+			+ p.getY() * Math.cos(newAngle)));
+	Position shifted = new Position(rotated.getX() + origin.getX(),
+		rotated.getY() + origin.getY());
+
+	return new Vector(origin, shifted);
+    }
+
+    /**
+     * @param rhs
+     * @return
+     */
+    public Vector sub(Vector rhs) {
+	return this.add(rhs.scale(-1.0));
+    }
+
+    /**
+     * @return
+     */
+    public Position toPosition() {
+	return pos;
+    }
+
+    /**
+     * @param state
+     * @param c
+     */
+    public void draw(Game state, bwapi.Color c) {
+	state.drawLineMap(origin, pos, c);
+	state.drawCircleMap(pos, 2, c);
+	state.drawTextMap(toPosition(), "" + getAngle());
+    }
+
+    public String toString() {
+	return toPosition().toString();
+    }
 }
--- src/SommerinoCamperino_New.java
+++ src/SommerinoCamperino_New.java
@@ -12,281 +12,309 @@ import bwta.BaseLocation;
 
 public class SommerinoCamperino_New extends DefaultBWListener {
 
-	private Mirror mirror = new Mirror();
-	private Game game;
-	private Player self;
-	private BaseLocation enemyLoc;
-	private BaseLocation playerLoc;
+    private Mirror mirror = new Mirror();
+    private Game game;
+    private Player self;
+    private BaseLocation enemyLoc;
+    private BaseLocation playerLoc;
 
-	enum State {
-		INIT, NORMAL
+    enum State {
+	INIT, NORMAL
+    }
+
+    private State state = State.INIT;
+
+    public void run() {
+	mirror.getModule().setEventListener(this);
+	mirror.startGame();
+    }
+
+    @Override
+    public void onStart() {
+	state = State.INIT;
+	game = mirror.getGame();
+	self = game.self();
+	BWTA.readMap();
+	BWTA.analyze();
+
+	List<BaseLocation> locs = BWTA.getStartLocations();
+	Iterator<BaseLocation> it = locs.iterator();
+	playerLoc = BWTA.getStartLocation(self);
+
+	while (it.hasNext()) {
+	    BaseLocation l = it.next();
+	    if (l != playerLoc) {
+		enemyLoc = l;
+	    }
 	}
+    }
 
-	private State state = State.INIT;
+    protected void attackEverythingInSight() {
+	Player enemy = game.enemy();
+	List<Unit> units = self.getUnits();
 
-	public void run() {
-		mirror.getModule().setEventListener(this);
-		mirror.startGame();
-	}
+	HashSet<Unit> s = new HashSet<Unit>();
+	s.addAll(enemy.getUnits());
 
-	@Override
-	public void onStart() {
-		game = mirror.getGame();
-		self = game.self();
-		BWTA.readMap();
-		BWTA.analyze();
-		
-		List<BaseLocation> locs = BWTA.getStartLocations();
-		Iterator<BaseLocation> it = locs.iterator();
-		playerLoc = BWTA.getStartLocation(self);
-		
-		while (it.hasNext()) {
-			BaseLocation l = it.next();
-			if (l != playerLoc) {
-				enemyLoc = l;
-			}
+	units.sort(new Comparator<Unit>() {
+	    @Override
+	    public int compare(Unit o1, Unit o2) {
+		int n1 = game.getUnitsInRadius(o1.getPosition(),
+			o1.getType().groundWeapon().maxRange()).size();
+		int n2 = game.getUnitsInRadius(o1.getPosition(),
+			o1.getType().groundWeapon().maxRange()).size();
+		return n2 - n1;
+	    }
+	});
+
+	for (Unit u : units) {
+	    HashSet<Unit> newS = new HashSet<Unit>();
+	    List<Unit> unitsInRange = game.getUnitsInRadius(u.getPosition(),
+		    u.getType().groundWeapon().maxRange());
+
+	    if (unitsInRange.isEmpty()) {
+		continue;
+	    }
+
+	    for (Unit ru : unitsInRange) {
+		if (s.contains(ru)) {
+		    newS.add(ru);
 		}
+	    }
+
+	    if (!newS.isEmpty())
+		s = newS;
 	}
 
-	protected void attackEverythingInSight() {
-		Player enemy = game.enemy();
-		List<Unit> units = self.getUnits();
+	if (s.isEmpty())
+	    return;
 
-		HashSet<Unit> s = new HashSet<Unit>();
-		s.addAll(enemy.getUnits());
+	Unit eu = (Unit) s.iterator().next();
+	if (!eu.isVisible())
+	    return;
 
-		units.sort(new Comparator<Unit>() {
-			@Override
-			public int compare(Unit o1, Unit o2) {
-				int n1 = game.getUnitsInRadius(o1.getPosition(), o1.getType().groundWeapon().maxRange()).size();
-				int n2 = game.getUnitsInRadius(o1.getPosition(), o1.getType().groundWeapon().maxRange()).size();
-				return n2 - n1;
-			}
-		});
+	for (Unit myUnit : self.getUnits()) {
+	    if (myUnit.getLastCommandFrame() >= game.getFrameCount()
+		    || myUnit.isAttackFrame())
+		continue;
+	    if (myUnit.getLastCommand()
+		    .getUnitCommandType() == UnitCommandType.Attack_Unit)
+		continue;
+	    myUnit.attack(eu);
+	}
+    }
 
-		for (Unit u : units) {
-			HashSet<Unit> newS = new HashSet<Unit>();
-			List<Unit> unitsInRange = game.getUnitsInRadius(u.getPosition(), u.getType().groundWeapon().maxRange());
+    private boolean spreadUnits(Game state, List<Unit> units, int distance) {
+	if (units.size() < 2)
+	    return true;
 
-			if (unitsInRange.isEmpty()) {
-				continue;
-			}
+	List<Position> wantPositions = new LinkedList<Position>();
+	boolean requiredActions = false;
 
-			for (Unit ru : unitsInRange) {
-				if (s.contains(ru)) {
-					newS.add(ru);
-				}
-			}
+	for (int i = 0; i < units.size() - 1; i++) {
+	    Unit u0 = units.get(i);
+	    Unit closest = u0;
+	    int minDistance = distance;
+	    for (int j = i + 1; j < units.size(); j++) {
+		Unit u1 = units.get(j);
 
-			if (!newS.isEmpty())
-				s = newS;
+		int curDistance = u0.getDistance(u1);
+		if (curDistance >= distance)
+		    continue;
+
+		if (minDistance >= curDistance) {
+		    minDistance = curDistance;
+		    closest = u1;
 		}
+	    }
 
-		if (s.isEmpty())
-			return;
+	    Vector a = new Vector(u0.getPosition());
+	    Vector b = new Vector(closest.getPosition());
+	    Vector ab = new Vector(b, a);
 
-		Unit eu = (Unit) s.iterator().next();
-		if (!eu.isVisible())
-			return;
+	    int radius = Math.max(u0.getType().height(), u0.getType().width());
+	    ab = new Vector(a.toPosition(), a.add(ab).toPosition());
+	    ab = ab.scale(1.5);
 
-		for (Unit myUnit : self.getUnits()) {
-			if (myUnit.getLastCommandFrame() >= game.getFrameCount() || myUnit.isAttackFrame())
-				continue;
-			if (myUnit.getLastCommand().getUnitCommandType() == UnitCommandType.Attack_Unit)
-				continue;
-			myUnit.attack(eu);
+	    if (minDistance < distance)
+		requiredActions = true;
+
+	    int tries = 0;
+	    while (tries < 5) {
+		List<Unit> uir = state.getUnitsInRadius(ab.toPosition(),
+			radius);
+		if (uir.size() == 0) {
+		    if (u0.isIdle()) {
+			wantPositions.add(ab.toPosition());
+			u0.move(ab.toPosition());
+		    }
+		    break;
 		}
+		ab = ab.rotate(64);
+		tries++;
+	    }
+	}
+	for (Position pos : wantPositions) {
+	    state.drawCircleMap(pos, 2, bwapi.Color.White);
+	}
+	return requiredActions;
+    }
+
+    private Line unitAlignment = null;
+    private Line wantAlignment = null;
+
+    private boolean unitsAreAligned(Game state, List<Unit> units,
+	    Position pos) {
+	if (units.size() < 2)
+	    return true;
+
+	List<Position> positions = new LinkedList<Position>();
+	for (Unit u : units) {
+	    positions.add(u.getPosition());
 	}
 
-	private boolean spreadUnits(Game state, List<Unit> units, int distance) {
-		if (units.size() < 2)
-			return true;
+	unitAlignment = Line.fromObservations(positions);
 
-		List<Position> wantPositions = new LinkedList<Position>();
-		boolean requiredActions = false;
+	Unit u0 = units.get(0);
+	Unit u1 = units.get(units.size() - 1);
 
-		for (int i = 0; i < units.size(); i++) {
-			Unit u0 = units.get(i);
-			Unit closest = u0;
-			int minDistance = distance;
-			for (int j = i + 1; j < units.size(); j++) {
-				Unit u1 = units.get(j);
+	Position p0 = unitAlignment.at(u0.getX());
+	Position p1 = unitAlignment.at(u1.getX());
 
-				int curDistance = u0.getDistance(u1);
-				if (curDistance >= distance)
-					continue;
+	Vector align = new Vector(p0, p1);
+	Vector mid = align.scale(0.5);
 
-				if (minDistance >= curDistance) {
-					minDistance = curDistance;
-					closest = u1;
-				}
-			}
+	Vector dir;
+	if (pos != null)
+	    dir = new Vector(mid.toPosition(), pos);
+	else
+	    dir = new Vector(mid.toPosition(), mid.add(64, 64).toPosition());
 
-			Position wantPos = new Position(
-					u0.getX() - (closest.getX() - u0.getX()),
-					u0.getY() - (closest.getY() - u0.getY()));
+	dir.draw(state, Color.Purple);
+	Vector v1 = dir.rotate(90);
+	Vector v2 = dir.rotate(270);
 
-			wantPositions.add(wantPos);
+	wantAlignment = Line.fromPoints(v1, v2);
+	wantAlignment.draw(state, u0.getX(), u1.getX(), bwapi.Color.Green);
+	unitAlignment.draw(state, u0.getX(), u1.getX(), bwapi.Color.Yellow);
 
-			if (minDistance < distance)
-				requiredActions = true;
-			if (u0.isIdle()) {
-				Vector v0 = new Vector(u0.getPosition(), wantPos);
-				v0 = v0.rotate(Math.random() * 360);
-				u0.move(v0.toPosition());
-			}
-		}
-		int offset = 0;
-		for (Position pos : wantPositions) {
-			state.drawCircleMap(pos, 2, bwapi.Color.Cyan);
-		}
-		return requiredActions;
+	double slopeDelta = Math.abs(
+		wantAlignment.getSlope() - unitAlignment.getSlope());
+	double interceptDelta = Math.abs(
+		wantAlignment.getIntercept() - unitAlignment.getIntercept());
+	state.drawTextScreen(10, 50,
+		String.format("Slope-Delta: %f", slopeDelta));
+	return slopeDelta < 0.5 && interceptDelta < 10;
+    }
+
+    private void alignUnits(Game state, List<Unit> units) {
+	if (unitAlignment == null || wantAlignment == null)
+	    return;
+	for (Unit u : units) {
+	    if (!u.isIdle())
+		continue;
+
+	    Position u_pos = u.getPosition();
+
+	    double a_g = wantAlignment.getSlope();
+	    double b_g = wantAlignment.getIntercept();
+	    double a_f = (-1) * a_g;
+	    double b_f = u_pos.getY() - a_f * u_pos.getX();
+
+	    double x = (b_f - b_g) / (a_g - a_f);
+
+	    Position to = wantAlignment.at(x);
+	    Vector v = new Vector(to);
+	    v.draw(state, Color.Purple);
+	    u.move(to);
+	    
+	}
+    }
+
+    private void attackMoveTo(Game state, List<Unit> units, Position to) {
+	for (Unit u : units) {
+	    if (u.isIdle()) {
+		u.attack(new Position(u.getPosition().getX() + to.getX(),
+			u.getPosition().getY() + to.getY()));
+	    }
+	}
+    }
+
+    private void drawEnemies(Game state, Player enemy) {
+	for (Unit e : enemy.getUnits()) {
+	    if (!e.isVisible())
+		continue;
+	    state.drawBoxMap(e.getX() - 2, e.getY() - 2, e.getX() + 2,
+		    e.getY() + 2, bwapi.Color.Red);
+	}
+    }
+
+    private void drawOrders(Game state, List<Unit> units) {
+	for (Unit u : units) {
+	    if (u.isIdle())
+		continue;
+
+	    Position pos = u.getOrderTargetPosition();
+	    bwapi.Color c = bwapi.Color.Green;
+
+	    UnitCommandType ty = u.getLastCommand().getUnitCommandType();
+	    if (ty == UnitCommandType.Attack_Move
+		    || ty == UnitCommandType.Attack_Unit)
+		c = bwapi.Color.Red;
+
+	    game.drawLineMap(u.getPosition(), pos, c);
 	}
 
-	private Line unitAlignment = null;
-	private Line wantAlignment = null;
+    }
 
-	private boolean unitsAreAligned(Game state, List<Unit> units, Position pos) {
-		if (units.size() < 2)
-			return true;
+    @Override
+    public void onFrame() {
+	try {
+	    game.drawTextScreen(10, 10,
+		    "Playing as " + self.getName() + " - " + self.getRace());
+	    game.drawTextScreen(400, 10, "State: " + state);
 
-		units.sort(new Comparator<Unit>() {
-			@Override
-			public int compare(Unit o1, Unit o2) {
-				int comp = o1.getX() - o2.getX();
-				if (comp == 0)
-					comp = o1.getY() - o2.getY();
-				return comp;
-			}
-		});
-
-		List<Position> positions = new LinkedList<Position>();
-		for (Unit u : units) {
-			positions.add(u.getPosition());
+	    List<Unit> units = self.getUnits();
+	    units.sort(new Comparator<Unit>() {
+		@Override
+		public int compare(Unit o1, Unit o2) {
+		    Position origin = new Position(0, 0);
+		    int d1 = o1.getDistance(origin);
+		    int d2 = o2.getDistance(origin);
+		    return d1 - d2;
 		}
+	    });
 
-		unitAlignment = Line.fromObservations(positions);
+	    Player enemy = game.enemy();
+	    drawEnemies(game, enemy);
+	    drawOrders(game, units);
+	    
+	    switch (state) {
+	    case INIT:
+		Position pos = null;
+		if (enemyLoc != null)
+		    pos = enemyLoc.getPosition();
 
-		Unit u0 = units.get(0);
-		Unit u1 = units.get(units.size() - 1);
-
-		
-		Position p0 = unitAlignment.at(u0.getX());
-		Position p1 = unitAlignment.at(u1.getX());
-				
-		Vector align = new Vector(p0, p1);
-		Vector mid = align.scale(0.5);
-		
-		Vector dir;
-		if (pos != null)
-			dir = new Vector(mid.toPosition(), pos);
-		else
-			dir = new Vector(mid.toPosition(), mid.add(64, 64).toPosition());
-
-		Vector v1 = dir.rotate(90);
-		Vector v2 = dir.rotate(270);
-		
-		wantAlignment = Line.fromPoints(v1, v2);
-		wantAlignment.draw(state,  u0.getX(), u1.getX(), bwapi.Color.Green);
-		unitAlignment.draw(state, u0.getX(), u1.getX(), bwapi.Color.Yellow);
-		
-		double slopeDelta = Math.abs(wantAlignment.getSlope() - unitAlignment.getSlope());
-		state.drawTextScreen(10, 50, String.format("Slope-Delta: %f", slopeDelta));
-		return slopeDelta < 0.5;
+//		if (!unitsAreAligned(game, units, pos))
+//		    alignUnits(game, units);
+//		else {
+		    if (!spreadUnits(game, units, 30)) {
+//			state = State.NORMAL;
+		    }
+//		}
+		break;
+	    case NORMAL:
+		attackEverythingInSight();
+		attackMoveTo(game, units, new Position(0, 64));
+		break;
+	    }
+	} catch (Throwable t) {
+	    game.sendText("Exception caught");
+	    t.printStackTrace();
 	}
-	
-	private void alignUnits(Game state, List<Unit> units) {
-		if (unitAlignment == null || wantAlignment == null)
-			return;
-		for (Unit u : units) {
-			if (!u.isIdle())
-				continue;
-			
-			
-			Position u_pos = u.getPosition();
+    }
 
-			double a_g = wantAlignment.getSlope();
-			double b_g = wantAlignment.getIntercept();
-			double a_f = (-1) * a_g;
-			double b_f = u_pos.getY() - a_f * u_pos.getX();
-
-			double x = (b_f - b_g) / (a_g - a_f);
-			
-			Position to = wantAlignment.at(x);
-			u.move(to);
-		}
-	}
-
-	private void attackMoveTo(Game state, List<Unit> units, Position to) {
-		for (Unit u : units) {
-			if (u.isIdle()) {
-				u.attack(new Position(u.getPosition().getX() + to.getX(), u.getPosition().getY() + to.getY()));
-			}
-		}
-	}
-
-	private void drawEnemies(Game state, Player enemy) {
-		for (Unit e : enemy.getUnits()) {
-			if (!e.isVisible())
-				continue;
-			state.drawBoxMap(e.getX() - 2, e.getY() - 2, e.getX() + 2, e.getY() + 2, bwapi.Color.Red);
-		}
-	}
-
-	private void drawOrders(Game state, List<Unit> units) {
-		for (Unit u : units) {
-			if (u.isIdle())
-				continue;
-
-			Position pos = u.getOrderTargetPosition();
-			bwapi.Color c = bwapi.Color.Green;
-
-			UnitCommandType ty = u.getLastCommand().getUnitCommandType();
-			if (ty == UnitCommandType.Attack_Move || ty == UnitCommandType.Attack_Unit)
-				c = bwapi.Color.Red;
-
-			game.drawLineMap(u.getPosition(), pos, c);
-		}
-
-	}
-
-	@Override
-	public void onFrame() {
-		try {
-			game.drawTextScreen(10, 10, "Playing as " + self.getName() + " - " + self.getRace());
-			game.drawTextScreen(400, 10, "State: " + state);
-
-			Player enemy = game.enemy();
-			drawEnemies(game, enemy);
-			drawOrders(game, self.getUnits());
-
-			
-			switch (state) {
-			case INIT:
-				Position pos = null;
-				if (enemyLoc != null)
-					pos = enemyLoc.getPosition();
-				
-				if (!unitsAreAligned(game, self.getUnits(), pos))
-					alignUnits(game, self.getUnits());
-				else {
-					if (!spreadUnits(game, self.getUnits(), 15)) {
-						state = State.NORMAL;
-					}
-				}
-				break;
-			case NORMAL:
-				attackEverythingInSight();
-				attackMoveTo(game, self.getUnits(), new Position(0, 15));
-				break;
-			}
-		} catch (Throwable t) {
-			game.sendText("Exception caught");
-			t.printStackTrace();
-		}
-	}
-
-	public static void main(String[] args) {
-		new SommerinoCamperino_New().run();
-	}
+    public static void main(String[] args) {
+	new SommerinoCamperino_New().run();
+    }
 }
