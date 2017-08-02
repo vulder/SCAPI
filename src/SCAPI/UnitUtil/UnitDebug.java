d304f53304b21d8b2fa53c17cd7d086d4a4cdc10
Florian Sattler <vuld3r@gmail.com>
2017-08-02 17:09:02 +0200

    added cmd debugging

--- src/SCAPI/UnitUtil/UnitDebug.java
+++ src/SCAPI/UnitUtil/UnitDebug.java
@@ -3,6 +3,7 @@ package SCAPI.UnitUtil;
 import bwapi.Game;
 import bwapi.Player;
 import bwapi.Unit;
+import bwapi.UnitCommand;
 
 public class UnitDebug {
 
@@ -32,6 +33,32 @@ public class UnitDebug {
         }
     }
 
+    /**
+     * Draws unit command debug data to the screen.
+     * 
+     * @param game
+     * @param yOffset
+     */
+    public static void drawUnitCommands(Game game) {
+        drawUnitCommands(game, 0);
+    }
+    
+    /**
+     * Draws unit command debug data to the screen.
+     * 
+     * @param game
+     * @param yOffset
+     */
+    public static void drawUnitCommands(Game game, int yOffset) {
+        Player self = game.self();
+        
+        for (Unit myUnit : self.getUnits()) {
+            game.drawTextScreen(10, 20 + yOffset, unitCommandStr(myUnit));
+            yOffset += 10;
+        }
+        
+    }
+
     private static String unitToFancyString(Unit unit) {
         StringBuilder SB = new StringBuilder();
         Unit target = unit.getOrderTarget();
@@ -46,6 +73,28 @@ public class UnitDebug {
                     target.getType().toString(), target.getHitPoints(), target.getType().maxHitPoints()));
         }
 
+        return SB.toString();
+    }
+    
+    private static String unitCommandStr(Unit unit) {
+        StringBuilder SB = new StringBuilder();
+        
+        UnitCommand cmd = unit.getLastCommand();
+        
+        String targetStr = "nothing";
+        
+        if (cmd.getTarget() != null) {
+            targetStr = cmd.getTarget().toString();
+        }
+        if (cmd.getTargetPosition() != null) {
+            targetStr = cmd .getTargetPosition().toString();
+        }
+
+        String cmdString = String.format("%14s -> %s", cmd.getUnitCommandType(), targetStr);
+        
+        
+        SB.append(String.format("ID: %2d doing: %s", unit.getID(), cmdString));
+        
         return SB.toString();
     }
 }
